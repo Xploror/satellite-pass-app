8216ed87a680739d71354981cd8779fc05657500
Xploror <suryansharyan01@gmail.com>
2026-08-30 15:14:24 -0400

    minor fix: terrestre error handling

--- lib/providers/terrestre.py
+++ lib/providers/terrestre.py
@@ -26,9 +26,12 @@ def fetch_API(sat_objs: list, lab_obj, limit: int = 1, visible_only: bool = Fals
                                             timeout=3)
                 response.raise_for_status()
                 data = response.json()
-                timestamp_interval = [float(data[0]['rise']['utc_timestamp']), float(data[0]['set']['utc_timestamp'])]
-                sat_obj.is_visible = time.time() > timestamp_interval[0] and time.time() < timestamp_interval[1]
-                break
+                if len(data) != 0:
+                    timestamp_interval = [float(data[0]['rise']['utc_timestamp']), float(data[0]['set']['utc_timestamp'])]
+                    sat_obj.is_visible = time.time() > timestamp_interval[0] and time.time() < timestamp_interval[1]
+                    break
+                else:
+                    pass
             except requests.exceptions.RequestException as e:
                 if attempt == 4:
                     raise ConnectionError(f"{url} unreachable!") from e
